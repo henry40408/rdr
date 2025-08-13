c85fda5c2016f3346280edc4451140c75976f6d8
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2025-08-13 13:40:37 +0800

    feat: add export OPML functionality to FeedsController and OpmlService

--- src/feeds.controller.ts
+++ src/feeds.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header } from '@nestjs/common';
 import { OpmlService } from './opml.service';
 import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 import { Category } from './entities';
@@ -12,5 +12,19 @@ export class FeedsController {
   @ApiOkResponse({ type: Category, isArray: true })
   feeds(): Category[] {
     return this.opmlService.categories;
+  }
+
+  @Get('export_opml')
+  @ApiOperation({ summary: 'Export feeds as OPML file' })
+  @ApiOkResponse({
+    type: String,
+    content: {
+      'application/xml': { schema: { type: 'string', format: 'binary' } },
+    },
+  })
+  @Header('Content-Type', 'application/xml')
+  @Header('Content-Disposition', 'attachment; filename="feeds.opml"')
+  exportOpml(): string {
+    return this.opmlService.exportOPML();
   }
 }
--- src/opml.service.ts
+++ src/opml.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Logger, OnApplicationBootstrap } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import fs from 'fs/promises';
-import { parseStringPromise } from 'xml2js';
+import { Builder, parseStringPromise } from 'xml2js';
 import { Category, Feed } from './entities';
 
 export interface OpmlOutlineFeed {
@@ -50,7 +50,7 @@ export class OpmlService implements OnApplicationBootstrap {
     );
     const opmlContent = await fs.readFile(opmlFilePath, 'utf-8');
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const parsed: OpmlParsed = await parseStringPromise(opmlContent);
 
     const body = parsed.opml.body[0];
@@ -72,5 +72,34 @@ export class OpmlService implements OnApplicationBootstrap {
     this.logger.log(
       `Loaded ${this.categories.length} categories from OPML file.`,
     );
+  }
+
+  exportOPML(): string {
+    const now = new Date();
+    const obj = {
+      opml: {
+        $: { version: '2.0' },
+        head: [
+          {
+            title: ['rdr'],
+            dateCreated: [now.toUTCString()],
+          },
+        ],
+        body: this.categories.map((category) => ({
+          $: { text: category.name },
+          outline: category.feeds.map((feed) => ({
+            $: {
+              title: feed.title,
+              text: feed.title,
+              xmlUrl: feed.xmlUrl,
+              htmlUrl: feed.htmlUrl,
+              type: 'rss',
+            },
+          })),
+        })),
+      },
+    };
+    const builder = new Builder();
+    return builder.buildObject(obj);
   }
 }
