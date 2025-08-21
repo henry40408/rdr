511057d69ab6aab0b6dd6cefd030dae015104a51
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2025-08-21 22:49:57 +0800

    feat: Add OPML processing with tracing and clap integration
    - Updated `.gitignore` to exclude OPML files.
    - Modified `Cargo.lock` to include new dependencies for OPML processing and tracing.
    - Updated `Cargo.toml` to add dependencies: anyhow, bon, clap, clio, no_color, opml, tracing, and tracing-subscriber.
    - Refactored `main.rs` to implement OPML file reading and structured logging using tracing.
    - Introduced `AppState` and `Opts` structs for managing application state and command-line arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::ExitCode;
+
+use bon::Builder;
+use clap::Parser;
+use clio::Input;
+use no_color::is_no_color;
+use opml::OPML;
+use tracing::{Level, debug, enabled};
+use tracing_subscriber::{EnvFilter, fmt::format::FmtSpan};
+
+#[derive(Builder, Debug)]
+struct AppState {
+    #[builder(into)]
+    opml: OPML,
+}
+
+#[derive(Debug, Parser)]
+struct Opts {
+    /// The input file to read from
+    #[clap(long, value_parser, default_value = "feeds.opml")]
+    file: Input,
+}
+
+fn try_main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt()
+        .with_ansi(!is_no_color())
+        .with_env_filter(EnvFilter::from_default_env())
+        .with_span_events(FmtSpan::CLOSE)
+        .compact()
+        .init();
+
+    let mut opts = Opts::parse();
+    let parsed = OPML::from_reader(&mut opts.file)?;
+    let app_state = AppState::builder().opml(parsed.clone()).build();
+
+    if enabled!(Level::DEBUG) {
+        for category in &app_state.opml.body.outlines {
+            for feed in &category.outlines {
+                let category = &category.text;
+                let feed = &feed.text;
+                debug!("Feed: {:?} (Category: {:?})", feed, category);
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn main() -> ExitCode {
+    if let Err(e) = try_main() {
+        eprintln!("Error: {}", e);
+        return ExitCode::FAILURE;
+    }
+    ExitCode::SUCCESS
 }
