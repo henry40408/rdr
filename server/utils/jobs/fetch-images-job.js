ad7b6ac34ecf99d27597e8f0cba53b69a2a8bcb7
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2025-11-28 02:33:41 +0000

    feat: implement batch processing for feed fetching in FetchEntriesJob and FetchImagesJob

--- server/utils/jobs/fetch-images-job.js
+++ server/utils/jobs/fetch-images-job.js
@@ -21,6 +21,8 @@ export class FetchImagesJob extends BaseJob {
     this.jobService = jobService;
     this.logger = logger;
     this.repository = repository;
+
+    this.BATCH_SIZE = 5;
   }
 
   /** @override */
@@ -41,25 +43,30 @@ export class FetchImagesJob extends BaseJob {
     logger.info("Starting feed images job");
 
     const users = await this.repository.findUsers();
-    const tasks = users.map(async (user) => {
+    for (const user of users) {
       const categories = await this.repository.findCategoriesWithFeed(user.id);
       const feeds = categories.flatMap((category) => category.feeds);
-      return await Promise.allSettled(
-        feeds.map(async (feed) => {
-          try {
-            return await this.feedService.fetchImage(user.id, feed);
-          } finally {
-            this.logger.debug({
-              msg: "Fetched image for feed",
-              feedId: feed.id,
-              counter: ++counter,
-              total: feeds.length,
-            });
-          }
-        }),
-      );
-    });
-    await Promise.allSettled(tasks);
+      for (let i = 0; i < feeds.length; i += this.BATCH_SIZE) {
+        const feedBatch = feeds.slice(i, i + this.BATCH_SIZE);
+
+        await Promise.allSettled(
+          feedBatch.map(async (feed) => {
+            try {
+              return await this.feedService.fetchImage(user.id, feed);
+            } finally {
+              this.logger.debug({
+                msg: "Fetched image for feed",
+                feedId: feed.id,
+                counter: ++counter,
+                total: feeds.length,
+              });
+            }
+          }),
+        );
+
+        await new Promise((resolve) => setImmediate(resolve));
+      }
+    }
 
     logger.info("Completed feed images job");
   }
