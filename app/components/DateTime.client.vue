800bf9099ef1ddd1f38e5244dfe9f6c82399783d
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2025-12-17 08:10:55 +0000

    feat: add DateTime and ExternalLink components; refactor EntryItem to utilize new components for improved structure and readability

---
+++ app/components/DateTime.client.vue
@@ -0,0 +1,11 @@
+<template>
+  <time :title="formatted" :datetime="formatted">{{ formatted }}</time>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  date: string;
+}>();
+
+const formatted = computed(() => new Date(props.date).toLocaleString());
+</script>
--- app/components/home/EntryItem.vue
+++ app/components/home/EntryItem.vue
@@ -1,30 +1,64 @@
 <template>
-  <q-item>
-    <q-item-section side>
-      <HomeEntryItemReadToggle :entry="entry" />
-    </q-item-section>
-    <q-item-section>
-      <q-item-label caption>
-        {{ feed.title }}
-        &middot;
-        {{ category.name }}
-        &middot;
-        <TimeAgo :date="entry.date" />
-      </q-item-label>
-      <q-item-label>
-        <span class="q-mr-xs">
-          <q-avatar v-if="imageExists" square size="xs">
-            <img :alt="`Feed image of ${feed.title}`" :src="`/api/images/external/${buildFeedImageKey(feed.id)}`" />
-          </q-avatar>
-          <q-icon v-else size="xs" name="rss_feed" />
-        </span>
-        {{ entry.title }}
-      </q-item-label>
-    </q-item-section>
-    <q-item-section top side>
-      <q-icon v-if="starred" size="xs" name="star" color="accent" />
-    </q-item-section>
-  </q-item>
+  <q-expansion-item group="entry" hide-expand-icon>
+    <template #header>
+      <q-item-section side>
+        <HomeEntryItemReadToggle :entry="entry" />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label caption>
+          {{ feed.title }}
+          &middot;
+          {{ category.name }}
+          &middot;
+          <TimeAgo :date="entry.date" />
+        </q-item-label>
+        <q-item-label>
+          <span class="q-mr-xs">
+            <q-avatar v-if="imageExists" square size="xs">
+              <img :alt="`Feed image of ${feed.title}`" :src="`/api/images/external/${buildFeedImageKey(feed.id)}`" />
+            </q-avatar>
+            <q-icon v-else size="xs" name="rss_feed" />
+          </span>
+          {{ entry.title }}
+        </q-item-label>
+      </q-item-section>
+      <q-item-section top side>
+        <q-icon v-if="starred" size="xs" name="star" color="accent" />
+      </q-item-section>
+    </template>
+
+    <template #default>
+      <q-card>
+        <q-card-section>
+          <span class="text-h6">
+            <ExternalLink :href="entry.link">
+              {{ entry.title }}
+            </ExternalLink>
+          </span>
+        </q-card-section>
+        <q-card-section>
+          <q-chip outline clickable @click="storeE.selectCategory(category.id)">
+            <q-avatar color="primary" text-color="white">
+              <q-icon name="category" />
+            </q-avatar>
+            Category: {{ category.name }}
+          </q-chip>
+          <q-chip outline clickable @click="storeE.selectFeed(category.id, feed.id)">
+            <q-avatar color="secondary" text-color="white">
+              <q-icon name="rss_feed" />
+            </q-avatar>
+            Feed: {{ feed.title }}
+          </q-chip>
+          <q-chip outline>
+            <q-avatar color="accent" text-color="white">
+              <q-icon name="calendar_today" />
+            </q-avatar>
+            Date:&nbsp;<DateTime :date="entry.date" />
+          </q-chip>
+        </q-card-section>
+      </q-card>
+    </template>
+  </q-expansion-item>
 </template>
 
 <script setup lang="ts">
@@ -33,6 +67,7 @@ const props = defineProps<{
     id: number;
     title: string;
     date: string;
+    link: string;
   };
   feed: {
     id: number;
