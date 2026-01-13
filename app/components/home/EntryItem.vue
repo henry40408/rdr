0f0ce81c0fe9f4e6d343e74126e8c732bcb13357
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2026-01-13 08:50:21 +0000

    feat: enhance EntryItem component with hover effects and toggle functionality for improved interactivity

--- app/components/home/EntryItem.vue
+++ app/components/home/EntryItem.vue
@@ -1,11 +1,11 @@
 <template>
   <div ref="entry" :class="{ 'bg-gray-300 dark:bg-black': isRead }" class="border-b last:border-b-0 border-b-gray-500">
     <div v-if="error" class="text-white bg-red-500 p-2">{{ error }}</div>
-    <div class="p-2 space-y-2">
+    <div class="p-2 space-y-2 hover:bg-gray-200 hover:dark:bg-gray-800 hover:cursor-pointer" @click="toggle">
       <div class="text-sm">
-        <a href="#" @click.prevent="entryStore.setFeed(feed.id, category.id)">{{ feed.title }}</a>
+        <a href="#" @click.prevent.stop="entryStore.setFeed(feed.id, category.id)">{{ feed.title }}</a>
         &middot;
-        <a href="#" @click.prevent="entryStore.setCategory(category.id)">{{ category.name }}</a>
+        <a href="#" @click.prevent.stop="entryStore.setCategory(category.id)">{{ category.name }}</a>
         &middot; <DurationToNow :datetime="entry.date" />
       </div>
       <div class="flex items-center gap-2">
@@ -24,20 +24,17 @@
           >{{ entry.title }}</ExternalLink
         >
       </div>
-      <details :open="open">
-        <summary @click.prevent="toggle">content</summary>
-        <div class="mt-2 space-y-2">
-          <div>
-            <button v-if="!['success', 'error'].includes(fullContentStatus)" class="x-button" @click="loadFullContent">
-              {{ fullContentStatus === "pending" ? "loading..." : "full content" }}
-            </button>
-            <button v-else class="x-button x-revert" @click="fullContentStatus = 'idle'">revert original</button>
-          </div>
-          <div>
-            <MarkedText class="x-content" :text="mergedContent" />
-          </div>
-        </div>
-      </details>
+    </div>
+    <div v-if="open" class="p-2 space-y-2">
+      <div>
+        <button v-if="!['success', 'error'].includes(fullContentStatus)" class="x-button" @click="loadFullContent">
+          {{ fullContentStatus === "pending" ? "loading..." : "full content" }}
+        </button>
+        <button v-else class="x-button x-revert" @click="fullContentStatus = 'idle'">revert original</button>
+      </div>
+      <div>
+        <MarkedText class="x-content" :text="mergedContent" />
+      </div>
     </div>
   </div>
 </template>
